f54265cf34b20f31e79eb0739895d02ad8c79e72
sofiquldev <sofiquldev@gmail.com>
2024-11-02 18:56:25 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import ECommerce from "@/components/Dashboard/E-commerce";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata = {
+  title: "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
+}
